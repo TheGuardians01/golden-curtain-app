4723cdea986a15b652930cf957828da00d0afd51
Joey <theguardians@thegoldencurtain.com>
2025-11-20 00:17:48 -0600

    tailwind adjustment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
-/ ** @type {import('tailwindcss').Config} * /
-module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: [],
 };
+
+export default config;
